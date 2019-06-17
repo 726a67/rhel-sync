155f6dba4d15ac07d0ade42cac1960b5f94c9eac
726a67 <726a67@gmail.com>
2019-06-17 11:43:09 -0500

    Update sync-maven.bash

--- maven/scripts/sync-maven.bash
+++ maven/scripts/sync-maven.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MAVEN_PROJECTS_DIR=/root/bridgegap/maven/maven-projects
+MAVEN_PROJECTS_DIR=/root/airbridge/maven/maven-projects
 
 #for PROJECT in `ls ${MAVEN_PROJECTS_DIR}` ; do echo "[${PROJECT}]"; echo -e ; cd ${MAVEN_PROJECTS_DIR}/${PROJECT}; mvn clean -U -q; mvn -U dependency:go-offline; mvn package -Dmaven.repo.local=${MAVEN_PROJECTS_DIR}/${PROJECT}; echo -e; done
 for PROJECT in `ls ${MAVEN_PROJECTS_DIR}` ; do echo "[${PROJECT}]"; echo -e ; cd ${MAVEN_PROJECTS_DIR}/${PROJECT}; mvn clean -U -q; mvn -U dependency:go-offline; echo -e; done
