80c84dcf2d5d9e0439fe754ef5804ea50a4ff6c4
root <root@artifactory7.goldberg.af.mil>
2019-11-26 16:28:25 -0500

    update sync-maven.bash

--- maven/scripts/sync-maven.bash
+++ maven/scripts/sync-maven.bash
@@ -2,6 +2,6 @@
 
 MAVEN_PROJECTS_DIR=/root/airbridge/maven/maven-projects
 
-#for PROJECT in `ls ${MAVEN_PROJECTS_DIR}` ; do echo "[${PROJECT}]"; echo -e ; cd ${MAVEN_PROJECTS_DIR}/${PROJECT}; mvn clean -U -q; mvn -U dependency:go-offline; mvn package -Dmaven.repo.local=${MAVEN_PROJECTS_DIR}/${PROJECT}; echo -e; done
-for PROJECT in `ls ${MAVEN_PROJECTS_DIR}` ; do echo "[${PROJECT}]"; echo -e ; cd ${MAVEN_PROJECTS_DIR}/${PROJECT}; mvn clean -U -q; mvn -U dependency:go-offline; echo -e; done
-cd
+for PROJECT in `ls ${MAVEN_PROJECTS_DIR}` ; do echo "[${PROJECT}]"; echo -e ; cd ${MAVEN_PROJECTS_DIR}/${PROJECT}; mvn clean dependency:go-offline package; echo -e; done
+
+cd ~
