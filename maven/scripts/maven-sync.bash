a01a1e41085d78d3ed1a784ccdbfdf8eaf8b36df
root <root@artifactory7.goldberg.af.mil>
2019-12-04 12:59:13 -0500

    rework directory structure

---
+++ maven/scripts/maven-sync.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MAVEN_PROJECTS_DIR=/root/airbridge/maven/projects
+
+for PROJECT in `ls ${MAVEN_PROJECTS_DIR}` ; do echo "[${PROJECT}]"; echo -e ; cd ${MAVEN_PROJECTS_DIR}/${PROJECT}; mvn -U clean dependency:go-offline package; echo -e; done
+
+cd ~
